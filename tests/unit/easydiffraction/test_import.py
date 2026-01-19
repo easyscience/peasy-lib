ce32ec55289734f79bbe69bdffd53fc3b3f1d539
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-01-19 09:35:18 +0100

    Update CI checks and revise test imports

--- tests/unit/easydiffraction/test_import.py
+++ tests/unit/easydiffraction/test_import.py
@@ -2,12 +2,3 @@ def test_import_easydiffraction():
     import easydiffraction
 
     assert easydiffraction is not None
-
-
-def test_dummy_function():
-    from easydiffraction.dummy import dummy_function
-
-    result = dummy_function()
-
-    assert result == "Hello from easydiffraction"
-    assert isinstance(result, str)
