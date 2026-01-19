167a91474c58f8acb7e0adfede1b70a5ca7cc8f3
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-01-19 18:39:12 +0100

    Add SPDX headers

--- src/easypeasy/__init__.py
+++ src/easypeasy/__init__.py
@@ -1,1 +1,3 @@
+# SPDX-FileCopyrightText: 2021-2026 EasyPeasy contributors <https://github.com/easyscience>
+# SPDX-License-Identifier: BSD-3-Clause
 """EasyPeasy library <https://easyscience.github.io/peasy-lib>."""
