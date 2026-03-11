dd8403473deb31e72e4ac182a1963156f0ada716
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-03-11 14:21:18 +0100

    Add utils docs and utility functions

---
+++ src/easypeasy/utils/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2025 EasyScience contributors <https://github.com/easyscience>
+# SPDX-License-Identifier: BSD-3-Clause
+"""EasyPeasy library."""
--- src/easypeasy/utils/utils.py
+++ src/easypeasy/utils/utils.py
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: 2026 EasyScience contributors <https://github.com/easyscience>
 # SPDX-License-Identifier: BSD-3-Clause
+"""Utility functions for EasyPeasy."""
 
 from __future__ import annotations
 
@@ -13,10 +14,37 @@ def dummy_method(
 
     Args:
         left (str): Left side of the new string.
+        right (bool): Right side of the new string.
+        middle (int): Middle part of the new string.
+
+    Returns:
+        str: The output string.
+    """
+
+    output = left + right
+    return output
+
+
+def dummy_method_2(left, right):
+    """Dummy method for testing. It concatenates the left and right
+    strings.
+
+    Args:
+        left (bool): Left side of the new string.
         right (str): Right side of the new string.
 
     Returns:
         str: The output string.
+    """
+
+    output = left + right
+    return output
+
+
+def dummy_method_3(left, right):
+    """Dummy method for testing.
+
+    It concatenates the left and right strings.
     """
 
     output = left + right
