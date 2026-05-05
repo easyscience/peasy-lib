0bfa0f43f7b79f5721fdfd4883dbae01d19482b9
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2026-05-05 20:51:20 +0200

    Fix documentation build (#34)
    * Enable pydoclint and format-docstring
    
    * Bump dependencies

--- src/easypeasy/utils/utils.py
+++ src/easypeasy/utils/utils.py
@@ -13,15 +13,22 @@ def dummy_method(
     left: str,
     right: str,
 ) -> str:
-    """Dummy method for testing. It concatenates the left and right
-    strings.
+    """
+    Dummy method for testing.
 
-    Args:
-        left (str): Left side of the new string.
-        right (str): Right side of the new string.
+    It concatenates the left and right strings.
 
-    Returns:
-        str: The output string.
+    Parameters
+    ----------
+    left : str
+        Left side of the new string.
+    right : str
+        Right side of the new string.
+
+    Returns
+    -------
+    str
+        The output string.
     """
 
     logger.debug('Calling dummy method')
